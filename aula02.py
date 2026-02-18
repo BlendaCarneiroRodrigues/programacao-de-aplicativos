3cdbabaa3bd29557402c1c9e8323cc0324ab5971
unknown <carneirorodriguesblenda4@gmail.com>
2026-02-18 12:44:54 -0300

    primeira aula de programação

---
+++ aula02.py
@@ -0,0 +1,10 @@
+numero = 1424 # variavel int
+numero_quebrado = 0.91 # variavel float
+toda_segunda_tem_aula_de_fisica = True # variavel booleana
+texto = "ta fazendo muito calor" # variavel de texto 
+
+
+print(numero)
+print(numero_quebrado)
+print(toda_segunda_tem_aula_de_fisica)
+print(texto)
