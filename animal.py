8be805749b59b1a7a8f54b015dd6f6718112e553
Aluno <carneirorodriguesblenda4@gmail>
2026-02-19 10:08:01 -0300

    aula pratica

---
+++ animal.py
@@ -0,0 +1,10 @@
+nome = "cavalo" # variavl para nome do animal (string)
+idade = 11 # variavel para idade (int)
+peso = 435 # variavel para peso (float)
+sexo = "masculino" # variavel para sexo (string)
+
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo)
