67d9536e54e1450fd7d636e653d33951dcde0db6
blenda carneiro rodrigues <carneirorodriguesblenda4@gmail.com>
2026-03-11 11:58:03 -0300

    aula 11/03

--- distribuicao.py
+++ distribuicao.py
@@ -1,5 +1,10 @@
 codigo_do_pacote = int(input("digite o codigo do pacote: "))
 peso_do_pacote = float(input("digite o peso do pacote: "))
 
-if peso_do_pacote < 5 and codigo_do_pacote % 10 == 0:
-    
+if peso_do_pacote > 50:
+    print(f"pacote {codigo_do_pacote}: carga pessada!")
+
+elif peso_do_pacote < 5 and codigo_do_pacote % 10 == 0:
+    print(f"pacote {codigo_do_pacote}: entrega expressa")
+else:
+    print("seu pacote não entra nas condições.")    
