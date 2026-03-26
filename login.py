8edf3491124a60845ebdf3601eae6dff5b96dda3
blenda carneiro rodrigues <carneirorodriguesblenda4@gmail.com>
2026-03-26 10:04:05 -0300

    aula 26/03

---
+++ login.py
@@ -0,0 +1,5 @@
+senha = int(input("Digite a senha de usuário"))
+senha_correta = 2009
+
+while senha != senha_correta:
+    senha
---
+++ somadeestoque.py
@@ -0,0 +1,7 @@
+numeros = [2.50, 15, 6.75, 1.40, 9.90]
+
+soma = 0 
+
+for valor in numeros:
+    soma += valor
+    print(f"A soma total da lista de numeros é: {soma}")
