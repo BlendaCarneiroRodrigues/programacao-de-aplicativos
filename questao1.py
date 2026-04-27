aa07dbaed53689743bd296511ec64392ef5f0a87
blenda carneiro rodrigues <carneirorodriguesblenda4@gmail.com>
2026-04-27 12:40:48 -0300

    aula 25/04

---
+++ questao1.py
@@ -0,0 +1,13 @@
+def avaliar_desempenho(notas):
+    if notas >= 9:
+        return "Exelente"
+    elif notas >= 7:
+        return "bom"
+    elif notas > 5:
+        return "regular"
+    else:
+        return "Insuficiente"
+    
+valor_das_notas = float(input("Digite uma nota de 0 a 10: ")) 
+resultado = avaliar_desempenho(notas)
+print(f"O resultado é {resultado}")
---
+++ questao2.py
@@ -0,0 +1,4 @@
+def senha_valida(senhas):
+    return len(senhas) >= 6
+while True:
+    senha = input("Digite a senha para verificação: ")
