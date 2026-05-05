ce1958c4f7790bf31b2c02bd60b9532d0b04f8c1
blenda carneiro rodrigues <carneirorodriguesblenda4@gmail.com>
2026-05-05 11:56:26 -0300

    05/05

--- questao6.py
+++ questao6.py
@@ -1,1 +1,6 @@
-def converter_km_para_ms (km/h):
+def converter_km_para_ms(quilometros_por_hora):
+    converte = quilometros_por_horas / 3.6
+    velocidade = int(input("Digite a velocidade: "))
+    return velocidade
+if converte < 80:
+    
