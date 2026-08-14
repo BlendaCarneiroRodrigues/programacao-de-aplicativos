77db45c1cffa68392d1bd02c47c45905dd7577b2
blenda carneiro rodrigues <carneirorodriguesblenda4@gmail.com>
2026-08-14 08:11:52 -0300

    prova 2 trimestre

--- prova-2-trimestre.py
+++ prova-2-trimestre.py
@@ -37,41 +37,54 @@ def cadastrar_telecom():
         cursor = conexao.cursor()
 
         nome_provedor = input("Digite o nome do provedor de internet: ")
-        outorga_anatel = input("Digite o código do outorga da anatel: ")
+        outorga_anatel = input("Digite o código da outorga da anatel: ")
 
-        comando_inserir = f''' INSERT INTO telecomunicacoes (nome_provedor, outorga_anatel)
-                                VALUES ('{nome_provedor}', '{outorga_anatel}')'''
-        
-        cursor.execute(comando_inserir)
-        conexao.commit
-        print("Telecomunicações cadastrada")
-    except sqlite3.Error as t:
-        print(f"Erro ao cadastrar telecomunicações: {t}")
-    finally:
+        cursor.execute("""
+            INSERT INTO telecomunicacoes (nome_provedor, outorga_anatel)
+            VALUES (?, ?)
+        """, (nome_provedor, outorga_anatel))
+
+        conexao.commit()
+
+        print("Provedor cadastrado com sucesso!")
+
+        cursor.execute("SELECT * FROM telecomunicacoes")
+        registros = cursor.fetchall()
+
+        print(f"O banco possui: {registros}")
         conexao.close()
 
+    except sqlite3.Error as erro:
+        print("Erro ao cadastrar provedor:", erro)
+
 def listar_telecom():
-    try: 
+    try:
         conexao = sqlite3.connect('provedor_internet.db')
         cursor = conexao.cursor()
 
         cursor.execute("SELECT * FROM telecomunicacoes")
         registros = cursor.fetchall()
 
+        print(f"Registros encontrados: {registros}")
+
         if not registros:
             print("Nenhum provedor cadastrado!")
         else:
             for registro in registros:
-                print(f"ID: {registros[0]}, Provedor: {registros[1]}, Outorga ANATEL: {registros[2]}") 
+                print(
+                    f"ID: {registro[0]}, "
+                    f"Provedor: {registro[1]}, "
+                    f"Outorga ANATEL: {registro[2]}"
+                )
+
+        conexao.close()
 
     except sqlite3.Error as erro:
-        print("Erro ao fazer listagem:", erro)
-    except Exception as erro:
-        print("Erro na listagem:", erro)
+        print("Erro ao listar provedor:", erro)
 
 def atualizar_telecom():
     try:
-        conexao = sqlite3.connect('provador_internet.db')
+        conexao = sqlite3.connect('provedor_internet.db')
         cursor = conexao.cursor()
 
         id_busca_telecom = int(input("Digite o número do ID que deseja atualizar: "))
@@ -86,7 +99,7 @@ def atualizar_telecom():
             novo_nome_provedor = input("Digite o novo nome do provedor de internet: ")
             novo_outorga_anatel = input("Digite o novo código do outorga da anatel: ")
 
-            comando = f''' UPDATE telecominicacoes SET  nome_provedor = '{novo_nome_provedor}', outorga_anatel = '{novo_outorga_anatel}' WHERE id = {id_busca_telecom}'''
+            comando = f''' UPDATE telecomunicacoes SET  nome_provedor = '{novo_nome_provedor}', outorga_anatel = '{novo_outorga_anatel}' WHERE id = {id_busca_telecom}'''
 
             cursor.execute(comando)
             print("Provedor atualizado!")
@@ -128,7 +141,7 @@ def cadastrar_central():
         bairro_central = input("Digite o bairro da crentral: ")
         id_telecom = int(input("Digite o id o provedor: "))
 
-        comando = f''' INSERT INTO centrais_distribuicao (bairro_central, id) VALUES ('{bairro_central}', '{id_telecom}')'''
+        comando = f''' INSERT INTO centrais_distribuicao (bairro_central, id_telecom) VALUES ('{bairro_central}', '{id_telecom}')'''
         
         cursor.execute(comando)
         conexao.commit()
@@ -136,9 +149,9 @@ def cadastrar_central():
     except ValueError:
         print("O ID deve ser um número.")
     except sqlite3.Error as erro:
-        print("Erro no banco de dados:", erro)
+        print("Erro no banco de dados ao cadastrar central:", erro)
     except Exception as erro:
-        print("Erro:", erro)
+        print("Erro ao cadastrar central:", erro)
     conexao.close()
 
 def listar_central():
@@ -146,26 +159,26 @@ def listar_central():
         conexao = sqlite3.connect('provedor_internet.db') 
         cursor = conexao.cursor()
 
-        cursor.execute("SELCT * FROM central_distribuicao")
+        cursor.execute("SELECT * FROM centrais_distribuicao")
         registros = cursor.fetchall()
 
         if not registros:
             print("Nenhuma central cadastrada!")  
         else:
             for registro in registros:
-                print(f"ID: {registro[0]}, Bairro central: {registro[1]}, Id provedor: {registro[2]}, Provedor: {registro[3]}") 
+                print(f"ID: {registro[0]}, Bairro central: {registro[1]}, Id provedor: {registro[2]}") 
     except sqlite3.Error as erro:
-        print("Erro no banco de dados:", erro)
+        print("Erro no banco de dados ao listar central:", erro)
     except Exception as erro:
-        print("Erro:", erro)
+        print("Erro ao listar a central:", erro)
 
 def atualizar_central():
     try:
         conexao = sqlite3.connect('provedor_internet.db')
         cursor = conexao.cursor()
 
-        id_busca = int(input("Digite da central que deseja atualizar: "))
-        cursor.execute(f''' SELECT * FROM central_distribuicao WHERE id = {id_busca}''')
+        id_busca = int(input("Digite o id da central que deseja atualizar: "))
+        cursor.execute(f''' SELECT * FROM centrais_distribuicao WHERE id = {id_busca}''')
         centrais = cursor.fetchone()
 
         if not centrais:
@@ -175,18 +188,19 @@ def atualizar_central():
             novo_bairro = input("Digite o novo bairro da central: ")
             novo_id_provedor = int(input("Digite o novo id do provedor: "))
 
-            comando = f''' UPDATE central_distribuicao SET bairro_central = '{novo_bairro}', id = '{novo_id_provedor}' WHERE id = {id_busca}'''
-
+            comando = f''' UPDATE centrais_distribuicao SET bairro_central = '{novo_bairro}', id = '{novo_id_provedor}' WHERE id = {id_busca}'''
+            
+            print("Central alterada com sucesso!")
             cursor.execute(comando)
             conexao.commit()
             conexao.close()
-            conexao.close()
+           
     except ValueError:
         print("Os IDs devem ser números.")
     except sqlite3.Error as erro:
-        print("Erro no banco de dados:", erro)
+        print("Erro no banco de dados ao atualizar central:", erro)
     except Exception as erro:
-        print("Erro:", erro)
+        print("Erro ao atualizar central:", erro)
 
 def excluir_central():
     try:
@@ -196,15 +210,15 @@ def excluir_central():
         listar_central()
         id_busca_excluir = int(input("Digite o id da central que deseja excluir: "))
 
-        cursor.execute(f''' DELETE FROM central_distribuicao WHERE id = {id_busca_excluir}''')
+        cursor.execute(f''' DELETE FROM centrais_distribuicao WHERE id = {id_busca_excluir}''')
         conexao.commit()
         conexao.close()
     except ValueError:
         print("O ID deve ser um número.")
     except sqlite3.Error as erro:
-        print("Erro no banco de dados:", erro)
+        print("Erro no banco de dados ao excluir central:", erro)
     except Exception as erro:
-        print("Erro:", erro) 
+        print("Erro ao excluir central:", erro) 
 
 def menu():
     try:
@@ -221,7 +235,7 @@ def menu():
             print("8- Excluir Central")
             print("0- Sair do Sistema")
 
-            opcao = input("Escolha uma opçã0: ") 
+            opcao = input("Escolha uma opção: ") 
 
             if opcao == "1":
                 cadastrar_telecom()
